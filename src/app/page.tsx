1c6f1adf40acbc057f1bf06ffbd6b4242f16d1fe
spbovarnick <spbovarnick@gmail.com>
2025-10-15 22:17:58 -0700

    ready2rip?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@
 
 import ThumbLike16Regular from "./components/ThumbLike16Regular";
 import SmashThatLike from "./components/SmashThatLike";
+import { useRef } from "react";
 
 export default function Home() {
-  let strobeTimeout: number | null = null;
+  let strobeTimeout: number | string;
   const partyRock = new Audio('/audio/party_rock.mp3')
   let textColor: string = "black";
+  const smashRef = useRef<SVGSVGElement>(null);
+  let pulseAnimation: Animation | null = null;
 
   const getHue = () => Math.random() * 360;
 
@@ -18,28 +21,41 @@ export default function Home() {
   };
 
   const smashLike = (e: React.MouseEvent<HTMLButtonElement> | React.TouchEvent<HTMLButtonElement>) => {
-    e.preventDefault();
     partyRock.play();
     neonStrobe();
+    if (smashRef.current) {
+      pulseAnimation = smashRef.current.animate(
+        [
+          { transform: "scale(1)" },
+          { transform: "scale(.8)"},
+          { transform: "scale(1)" }
+        ],
+        { duration: 400, iterations: Infinity, easing: "ease-out" }
+      );
+    }
   };
 
   const reset = (e: React.MouseEvent<HTMLButtonElement> | React.TouchEvent<HTMLButtonElement>) => {
-    e.preventDefault();
     clearTimeout(strobeTimeout);
     document.body.style.backgroundColor = "white";
     partyRock.pause();
     partyRock.currentTime = 0;
+    pulseAnimation?.cancel();
+    pulseAnimation = null;
   }
+
+
 
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <SmashThatLike size="200" color={textColor}/>
+      <SmashThatLike ref={smashRef} size="400" color={textColor}/>
       <button
         onTouchStart={smashLike}
         onMouseDown={smashLike}
         onMouseUp={reset}
-        className="w-min cursor-pointer">
+        onMouseLeave={reset}
+        className="thumb-btn w-min cursor-pointer thumb transition-transform duration-300 hover:scale-110 active:scale-80">
         <ThumbLike16Regular size="100"/>
       </button>
     </div>
